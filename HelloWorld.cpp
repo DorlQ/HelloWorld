299bbbd067a8b8714e6c2b4bfb0154b52586fdc2
DorlQ <36049884+DorlQ@users.noreply.github.com>
2018-09-08 15:58:31 +0300

    HelloWorld

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+int main ()
+{
+    using namespace std;
+cout <<"Hello World" << endl;
+system ("pause");
+return cout.good() ? EXIT_SUCCES : EXIT_FAILURE;
+
+
+}
+
+
